21164581616c3be66e17332342ba0a3fa400fa55
Shawn Brooks <mukymuk@gmail.com>
2019-01-05 11:36:26 -0600

    added board

---
+++ cw/platform.h
@@ -0,0 +1,3 @@
+#include <__debug_stdio.h>
+
+#define DBG(x) debug_printf x
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "global.h"
+#include "board.h"
+
+int main(int argc, char *argv[])
+{
+    DBG(("snd620\n"));
+
+
+
+
+    return 0;
+}
