3f837e0e5f180ccf81784508c78da3dc8da28c14
Shawn Brooks <mukymuk@gmail.com>
2019-01-14 10:05:40 -0600

    wip

--- global.h
+++ global.h
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 #include "platform.h"
 
 #define ARRAY_COUNT(x) (sizeof(x)/sizeof(*(x)))
--- mc.c
+++ mc.c
@@ -1,6 +1,45 @@
 #include "global.h"
 #include "mc.h"
 
+typedef struct _point_t
+{
+    float_t x, y;
+}
+point_t;
+
+typedef enum _motion_id_t
+{
+    motion_rapid, motion_line, motion_arc
+}
+motion_id_t;
+
+typedef struct _link_t
+{
+    motion_id_t     motion_id;
+    struct _geometry_t * p_next;
+}
+link_t;
+
+typedef struct _line_t
+{
+    link_t      link;
+    point_t     p;
+}
+line_t;
+
+typedef line_t rapid_t;
+
+typedef struct _arc_t
+{
+    link_t      link;
+    point_t     p;
+    point_t     center;
+
+}
+arc_t;
+
+
+
 typedef struct _mc_move_t
 {
     int32_t     delta;
