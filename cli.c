679a11b40865777e21ef3775dfa0972cffd1a21c
Shawn Brooks <mukymuk@gmail.com>
2019-07-31 08:53:43 -0500

    wip

--- cli.c
+++ cli.c
@@ -15,7 +15,7 @@
 
 #define ACTION_ASSIGNMENT   '='
 #define ACTION_QUERY        '?'
-#define ACTION_COMMAND      ' '
+#define ACTION_COMMAND      0
 
 
 typedef struct
@@ -27,6 +27,12 @@ typedef struct
     bool (*get)( void );
 }
 cmd_t;
+
+
+static cbuf_t * s_p_cbuf_write;
+static cbuf_t * s_p_cbuf_read;
+static lbuf_t   lbuf_cmd;
+static char lbuf_cmd_buffer[MAX_LINE_SIZE];
 
 static bool cmd_freq_get( void )
 {
@@ -67,6 +73,8 @@ static bool cmd_line( const char *p_args )
 {
 }
 
+static bool cmd_help( const char * p_args );
+
 static const cmd_t s_cmd[] =
 {
     { "freq", "<frequency in Hz>", "test step frequency", cmd_freq_set, cmd_freq_get },
@@ -74,14 +82,46 @@ static const cmd_t s_cmd[] =
     { "acc", "<axis #>, <speed,acc[,speed2,acc2,...]>", "velocity/acceleration profile for axis #", cmd_acc_set, cmd_acc_get },
     { "step", "<axis #>, <mm/step>","indicate distance per step for axis #", cmd_step_set, cmd_step_get },
 
-    { "line", "<x1, y1, x2, y2 [,speed]>", "constant speed move from x1,y1 to x2,y2", cmd_line, NULL }
+    { "line", "<x1, y1, x2, y2 [,speed]>", "constant speed move from x1,y1 to x2,y2", cmd_line, NULL },
+
+    { "help", NULL, NULL, cmd_help, NULL }
 
 };
 
-static cbuf_t * s_p_cbuf_write;
-static cbuf_t * s_p_cbuf_read;
-static lbuf_t   lbuf_cmd;
-static char lbuf_cmd_buffer[MAX_LINE_SIZE];
+#define SPACES 10
+
+static bool cmd_help( const char * p_args )
+{
+    static char * space = "          ";
+
+    uint8_t i;
+    uint32_t l;
+    for(i=0;i<ARRAY_COUNT(s_cmd);i++)
+    {
+        l = strlen( s_cmd[i].cmd );
+        cbuf_write( s_p_cbuf_write, s_cmd[i].cmd, l );
+        if( s_cmd[i].args || s_cmd[i].help )
+        {
+            cbuf_write( s_p_cbuf_write, space, SPACES - l - 2 );
+            cbuf_write_string( s_p_cbuf_write, "- " );
+            if( s_cmd[i].args )
+            {
+                cbuf_write_string( s_p_cbuf_write, s_cmd[i].args );
+                cbuf_write_string( s_p_cbuf_write, "\r\n" );
+            }
+            if( s_cmd[i].help )
+            {
+                cbuf_write( s_p_cbuf_write, space, SPACES );
+                cbuf_write_string( s_p_cbuf_write, s_cmd[i].help );
+                cbuf_write_string( s_p_cbuf_write, "\r\n" );
+            }
+        }
+        else
+        {
+            cbuf_write_string( s_p_cbuf_write, "\r\n" );
+        }
+    }
+}
 
 static void write_line( const char * p_str )
 {
@@ -117,24 +157,32 @@ void cli_init( cbuf_t * p_cbuf_read, cbuf_t * p_cbuf_write  )
     prompt();
 }
 
+#define TERMINATOR '\n'
+
 void cli_event(void)
 {
+
     uint8_t c;
+    char * p_cmd;
+
+    uint32_t size;
+    uint32_t i = 0;
+
     while( cbuf_read( s_p_cbuf_read, &c, sizeof(c) ) )
     {
         if( c == BACKSPACE )
         {
             if( lbuf_cmd.write_ndx )
             {
-                cbuf_write( s_p_cbuf_write, &c, sizeof(c) );
+                //cbuf_write( s_p_cbuf_write, &c, sizeof(c) );
                 lbuf_cmd.write_ndx--;
             }
             continue;
         }
-        cbuf_write( s_p_cbuf_write, &c, sizeof(c) );
+      //  cbuf_write( s_p_cbuf_write, &c, sizeof(c) );
         if( c == EOL )
         {
-            cbuf_write_byte( s_p_cbuf_write, LF );
+          //  cbuf_write_byte( s_p_cbuf_write, LF );
             lbuf_write_byte( &lbuf_cmd, 0 );
             char * p_cmd = skip_space( lbuf_cmd_buffer );
             char * p_action = skip_letters( p_cmd );
--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 #include "cli.h"
 #include "pmu.h"
 
-CBUF(s_usb_write,256);
-CBUF(s_usb_read,128);
+//CBUF(s_usb_write,256);
+//CBUF(s_usb_read,128);
 
 CBUF(s_uart_write,256);
-CBUF(s_uart_read,128);
+CBUF(s_uart_read,1);
 
 
 static uart_t s_uart;
@@ -19,19 +19,18 @@ void main(void)
 {
     bool a;
     board_init();
-    pmu_init();
-
-    CBUF_INIT( s_usb_read );
-    CBUF_INIT( s_usb_write );
+    
+    //CBUF_INIT( s_usb_read );
+    //CBUF_INIT( s_usb_write );
     CBUF_INIT( s_uart_read );
     CBUF_INIT( s_uart_write );
 
-    usbuart_init( &s_usb_read.cbuf, &s_usb_write.cbuf );
+    //usbuart_init( &s_usb_read.cbuf, &s_usb_write.cbuf );
 
-    uart_common_clock( CLKMAN_SCALE_DIV_8 );
+    uart_common_clock( CLKMAN_SCALE_DIV_1 );
     uart_init( &s_uart, &s_uart_read.cbuf, &s_uart_write.cbuf, BOARD_UART_DBG,
-               UART_CONFIG_8N1, UART_CONFIG_BAUD_SCALE8_115200 );
-    cli_init( &s_usb_read.cbuf, &s_usb_write.cbuf );
+               UART_CONFIG_8N1, UART_CONFIG_BAUD_DIV16 | 52 );
+    cli_init( &s_uart_read.cbuf, &s_uart_write.cbuf );
 
     void *p;
     uint32_t size;
@@ -43,8 +42,8 @@ void main(void)
     while( 1 )
     {
         board_sleep();
-  //      cli_event();
-
+        cli_event();
+/*
         if( size = cbuf_read_aquire( &s_uart_read.cbuf, &p ) )
         {
             cbuf_write( &s_usb_write.cbuf, p, size );
@@ -55,7 +54,7 @@ void main(void)
             cbuf_write( &s_uart_write.cbuf, p, size );
             cbuf_read_release( &s_usb_read.cbuf, size );
         }
-
+*/
     }
 }
 
